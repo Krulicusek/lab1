b61777d4c94bdb97e5079142f12c32a5d36c96be
kubii <kubawegrzynowski@gmail.com>
2022-10-11 01:40:58 +0200

    Add project files.

---
+++ lab1/Program.cs
@@ -0,0 +1,9 @@
+﻿using lab1;
+
+int randomNumbersCount = 10;
+RandomGenerator randomGenerator = new();
+List<double> randomList = randomGenerator.GetRandomNumbersList(randomNumbersCount);
+for (int i = 0; i < randomNumbersCount; i++)
+{
+    Console.WriteLine(randomList[i]);
+}
---
+++ lab1/RandomGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class RandomGenerator
+    {
+        #region Methods
+        public List<double> GetRandomNumbersList(int count)
+        {
+            List<double> numbers = new List<double>();  
+            for (int i = 0; i <= count; i++)
+            {
+               Random rand = new();
+               numbers.Add(rand.NextDouble());   
+            }
+            return numbers;
+        }
+        #endregion
+    }
+}
