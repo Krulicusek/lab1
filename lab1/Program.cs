f7b2e420d7bdf7e539ffa8bc5762b493f6325f17
unknown <kubawegrzynowski@gmail.com>
2022-10-11 15:07:22 +0200

    ex 3 ready

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -3,4 +3,16 @@
 int randomNumbersCount = 10;
 TemperatureSensorSimulator temperatureSensorSimulator = new();
 temperatureSensorSimulator.GetRandomNumbersList(randomNumbersCount);
+Console.WriteLine("Enter the count of numbers to print.");
+int number; 
+bool success = int.TryParse(Console.ReadLine(), out number);
+if (success && number >= 0)
+{
+    temperatureSensorSimulator.PrintCount = number;
+}
+else
+{
+    Console.WriteLine("There was an error trying to parse input, are you sure that you are providing a positive intiger value?");
+}
+
 Console.WriteLine(temperatureSensorSimulator.ToString());
--- lab1/TemperatureSensorSimulator.cs
+++ lab1/TemperatureSensorSimulator.cs
@@ -4,13 +4,17 @@ namespace lab1
 {
     internal class TemperatureSensorSimulator
     {
-        private List<double?> randomNumbersList = new();
         #region Members
+        private List<double?> randomNumbersList = new();
+        #endregion
+        #region Properties
         List<double?> RandomNumbersList
         {
             get => randomNumbersList;
             set => GetRandomNumbersList(10);
         }
+
+        public int PrintCount { get; set; }
         #endregion
         #region Methods
         public void GetRandomNumbersList(int count)
@@ -39,13 +43,23 @@ namespace lab1
             double readingAsDouble = (double) (reading + random.NextDouble());
             return readingAsDouble;
         }
+
+        public void SaveToFile()
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(Path.GetTempPath(), "plikDane.txt")))
+            {
+
+            }
+           
+        }
+
         public override string ToString()
         {
             StringBuilder stringBuilder = new();
             
-            foreach (double? number in randomNumbersList)
+            for (int i = 0; i < PrintCount; i++)
             {
-                stringBuilder.AppendLine(string.Format($"{number:F2}"));
+                stringBuilder.AppendLine(string.Format($"{RandomNumbersList[i]:F2}"));
             }
             return stringBuilder.ToString();
         }
