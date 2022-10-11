a00f629d2af2c154f254f7116fbdb30c7fed1aba
unknown <kubawegrzynowski@gmail.com>
2022-10-11 14:21:44 +0200

    exercise 2 ready

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,9 +1,6 @@
 ﻿using lab1;
 
 int randomNumbersCount = 10;
-RandomGenerator randomGenerator = new();
-List<double> randomList = randomGenerator.GetRandomNumbersList(randomNumbersCount);
-for (int i = 0; i < randomNumbersCount; i++)
-{
-    Console.WriteLine(randomList[i]);
-}
+TemperatureSensorSimulator temperatureSensorSimulator = new();
+temperatureSensorSimulator.GetRandomNumbersList(randomNumbersCount);
+Console.WriteLine(temperatureSensorSimulator.ToString());
---
+++ lab1/TemperatureSensorSimulator.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace lab1
+{
+    internal class TemperatureSensorSimulator
+    {
+        private List<double?> randomNumbersList = new();
+        #region Members
+        List<double?> RandomNumbersList
+        {
+            get => randomNumbersList;
+            set => GetRandomNumbersList(10);
+        }
+        #endregion
+        #region Methods
+        public void GetRandomNumbersList(int count)
+        {
+            List<double?> numbers = new List<double?>();
+            for (int i = 0; i <= count; i++)
+            {
+                Random rand = new();
+
+                numbers.Add(GetNewReading());
+            }
+            randomNumbersList =  numbers;
+        }
+        public double? GetNewReading()
+        {
+            Random random = new();
+            int reading = random.Next(-100, 101);
+            if (reading <= -80)
+            {
+                return null;
+            }
+            if (reading == 100)
+            {
+                return reading;
+            }
+            double readingAsDouble = (double) (reading + random.NextDouble());
+            return readingAsDouble;
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new();
+            
+            foreach (double? number in randomNumbersList)
+            {
+                stringBuilder.AppendLine(string.Format($"{number:F2}"));
+            }
+            return stringBuilder.ToString();
+        }
+        #endregion
+
+    }
+}
